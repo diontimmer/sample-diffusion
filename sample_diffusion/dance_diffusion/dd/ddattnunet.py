3a7d2f35873f3d37a19441811da206b9f65aaf7d
Dion Timmer <diontimmer@live.nl>
2023-07-08 12:51:03 -0400

    libbified baby

--- sample_diffusion/dance_diffusion/dd/ddattnunet.py
+++ sample_diffusion/dance_diffusion/dd/ddattnunet.py
@@ -2,17 +2,24 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from .blocks import SkipBlock, FourierFeatures, SelfAttention1d, ResConvBlock, Downsample1d, Upsample1d
+from .blocks import (
+    SkipBlock,
+    FourierFeatures,
+    SelfAttention1d,
+    ResConvBlock,
+    Downsample1d,
+    Upsample1d,
+)
 
 
 class DiffusionAttnUnet1D(nn.Module):
     def __init__(
-        self, 
-        global_args, 
-        io_channels = 2, 
-        depth=14, 
-        n_attn_layers = 6,
-        c_mults = [128, 128, 256, 256] + [512] * 10
+        self,
+        global_args,
+        io_channels=2,
+        depth=14,
+        n_attn_layers=6,
+        c_mults=[128, 128, 256, 256] + [512] * 10,
     ):
         super().__init__()
 
@@ -32,31 +39,27 @@ class DiffusionAttnUnet1D(nn.Module):
                 block = SkipBlock(
                     Downsample1d("cubic"),
                     conv_block(c_prev, c, c),
-                    SelfAttention1d(
-                        c, c // 32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c, c // 32) if add_attn else nn.Identity(),
                     conv_block(c, c, c),
-                    SelfAttention1d(
-                        c, c // 32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c, c // 32) if add_attn else nn.Identity(),
                     conv_block(c, c, c),
-                    SelfAttention1d(
-                        c, c // 32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c, c // 32) if add_attn else nn.Identity(),
                     block,
                     conv_block(c * 2 if i != depth else c, c, c),
-                    SelfAttention1d(
-                        c, c // 32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c, c // 32) if add_attn else nn.Identity(),
                     conv_block(c, c, c),
-                    SelfAttention1d(
-                        c, c // 32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c, c // 32) if add_attn else nn.Identity(),
                     conv_block(c, c, c_prev),
-                    SelfAttention1d(c_prev, c_prev //
-                                    32) if add_attn else nn.Identity(),
+                    SelfAttention1d(c_prev, c_prev // 32)
+                    if add_attn
+                    else nn.Identity(),
                     Upsample1d(kernel="cubic")
                     # nn.Upsample(scale_factor=2, mode='linear',
                     #             align_corners=False),
                 )
             else:
                 block = nn.Sequential(
-                    conv_block(io_channels + 16 + global_args.get('latent_dim'), c, c),
+                    conv_block(io_channels + 16 + global_args.get("latent_dim"), c, c),
                     conv_block(c, c, c),
                     conv_block(c, c, c),
                     block,
@@ -71,13 +74,16 @@ class DiffusionAttnUnet1D(nn.Module):
                 param *= 0.5
 
     def forward(self, input, t, cond=None):
-        timestep_embed = self.timestep_embed(t[:, None])[..., None].repeat([1, 1, input.shape[2]])
-        
+        timestep_embed = self.timestep_embed(t[:, None])[..., None].repeat(
+            [1, 1, input.shape[2]]
+        )
+
         inputs = [input, timestep_embed]
 
         if cond is not None:
-            cond = F.interpolate(cond, (input.shape[2], ), mode='linear', align_corners=False)
+            cond = F.interpolate(
+                cond, (input.shape[2],), mode="linear", align_corners=False
+            )
             inputs.append(cond)
 
         return self.net(torch.cat(inputs, dim=1))
-    
--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
+"""Install packages as defined in this file into the Python environment."""
 from setuptools import setup, find_packages
 
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+VERSION = {}
+
 setup(
-  name="sample-diffusion",
-  version="0.0.1",
-  description="",
-  packages=find_packages(),
-  install_requires=[
-    "torch",
-    "tqdm",
-  ]
+    name="Sample Diffusion",
+    author="sudosilico, mrhyazinth",
+    author_email="",
+    url="",
+    description="A Python library and CLI for generating audio samples using Harmonai Dance Diffusion models.",
+    version=VERSION.get("__version__", "0.0.1"),
+    packages=["sample_diffusion"],
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "tqdm",
+        "v-diffusion-pytorch",
+        "k-diffusion",
+        "pysoundfile",
+        "black",
+        "diffusers",
+        "transformers",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+    ],
 )
